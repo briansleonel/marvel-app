520a368adb8dbd275ce5848a789395235816a2ac
briansleonel <gonzalezbrianleonel76@gmail.com>
2021-10-12 23:03:38 -0300

    Creación del proyecto

---
+++ src/components/landingPage/landingPage.jsx
@@ -0,0 +1,18 @@
+import marvel from '../../assets/img/marvel.png';
+
+import styles from './landingPage.css'
+
+const LandingPage = () => {
+    return(
+        <section>
+            <div>
+                <img src={marvel} alt="Logo de Marvel" />
+            </div>
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+export default LandingPage;
